2182302189d918623372350dba965a756ef008be
pavelsuk <pavelsuk@users.noreply.github.com>
2021-04-05 20:03:19 +0200

    better handling of initialization"

--- 02_youtube_anobjectisa/foreground.js
+++ 02_youtube_anobjectisa/foreground.js
@@ -1,43 +1,53 @@
 console.log("foreground.js log")
 document.querySelector('.lnXdpd').classList.add('spinspinspin')
 
-const first = document.createElement('button');
-first.innerText = "SET DATA";
-first.id = "first";
+if (!document.getElementById("first")) {
+    injectButtons();
+}
 
-const second = document.createElement('button');
-second.innerText = "SHOUTOUT AND WAIT";
-second.id = "second";
+function injectButtons() {
+    console.log("injectButtons")
 
-const third = document.createElement('button');
-third.innerText = "Calculate interest";
-third.id = "third";
+    const first = document.createElement('button');
+    first.innerText = "SET DATA";
+    first.id = "first";
+    
+    const second = document.createElement('button');
+    second.innerText = "SHOUTOUT AND WAIT";
+    second.id = "second";
+    
+    const third = document.createElement('button');
+    third.innerText = "Calculate interest";
+    third.id = "third";
+    
+    const loandiv = document.createElement('div');
+    loandiv.innerText = "100";
+    loandiv.id = "loandiv";
+    
+    
+    document.querySelector('body').appendChild(first);
+    document.querySelector('body').appendChild(second);
+    document.querySelector('body').appendChild(third);
+    document.querySelector('body').appendChild(loandiv);
+    
+    first.addEventListener('click', () => {
+        chrome.storage.local.set({ "mykey": "mydata123" });
+        chrome.storage.local.set({ "loan": "100" });
+        console.log("I SET DATA");
+    });
+    
+    second.addEventListener('click', () => {
+        chrome.runtime.sendMessage({message: 'check the storage bro let me know'}, res => console.log(res));
+        console.log('I SENT THE MESSAGE')
+    });
+    
+    third.addEventListener('click', () => {
+        chrome.runtime.sendMessage({message: 'loan'});
+        console.log('LOAN')
+    });
+}
 
-const loandiv = document.createElement('div');
-loandiv.innerText = "100";
-loandiv.id = "loandiv";
 
-
-document.querySelector('body').appendChild(first);
-document.querySelector('body').appendChild(second);
-document.querySelector('body').appendChild(third);
-document.querySelector('body').appendChild(loandiv);
-
-first.addEventListener('click', () => {
-    chrome.storage.local.set({ "mykey": "mydata123" });
-    chrome.storage.local.set({ "loan": "100" });
-    console.log("I SET DATA");
-});
-
-second.addEventListener('click', () => {
-    chrome.runtime.sendMessage({message: 'check the storage bro let me know'}, res => console.log(res));
-    console.log('I SENT THE MESSAGE')
-});
-
-third.addEventListener('click', () => {
-    chrome.runtime.sendMessage({message: 'loan'});
-    console.log('LOAN')
-});
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     console.log(request.message);
