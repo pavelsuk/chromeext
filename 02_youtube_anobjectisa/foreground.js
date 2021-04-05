e9b26deef2bdc90c7cc543e08635cf09b291b4f6
pavelsuk <pavelsuk@users.noreply.github.com>
2021-04-05 19:38:29 +0200

    playing with buttons"

--- 02_youtube_anobjectisa/foreground.js
+++ 02_youtube_anobjectisa/foreground.js
@@ -13,9 +13,15 @@ const third = document.createElement('button');
 third.innerText = "Calculate interest";
 third.id = "third";
 
+const loandiv = document.createElement('div');
+loandiv.innerText = "100";
+loandiv.id = "loandiv";
+
+
 document.querySelector('body').appendChild(first);
 document.querySelector('body').appendChild(second);
 document.querySelector('body').appendChild(third);
+document.querySelector('body').appendChild(loandiv);
 
 first.addEventListener('click', () => {
     chrome.storage.local.set({ "mykey": "mydata123" });
@@ -35,5 +41,13 @@ third.addEventListener('click', () => {
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     console.log(request.message);
+    if (request.message === 'loan returned with interest') {
+        loan = 0;
+        chrome.storage.local.get("loan", value => {
+            console.log(value);
+            console.log(value.loan);
+            loandiv.innerText = "Total: " + value.loan.toFixed()  
+        });
+    }
 });
 
