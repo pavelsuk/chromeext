628f164b5caeb73522e05df369c001549768094b
pavelsuk <pavelsuk@users.noreply.github.com>
2021-04-05 16:47:43 +0200

    injecting buttons

--- 02_youtube_anobjectisa/foreground.js
+++ 02_youtube_anobjectisa/foreground.js
@@ -1,4 +1,15 @@
 console.log("foreground.js log")
-document.querySelector('.lnXdpd').classList.add('spinspinspin')
+// document.querySelector('.lnXdpd').classList.add('spinspinspin')
+
+const first = document.createElement('button');
+first.innerText = "SET DATA";
+first.id = "first";
+
+const second = document.createElement('button');
+second.innerText = "SHOUTOUT TO BACKEND";
+second.id = "second";
+
+document.querySelector('body').appendChild(first);
+document.querySelector('body').appendChild(second);
 
 
